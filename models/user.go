f645750662ffb7c22a225e5fc86a43e94942c5ff
Rori Kurniadi <rori.kurniadi@yahoo.com>
2016-11-29 00:26:15 +0700

    Add User & Task Model

--- models/user.go
+++ models/user.go
@@ -2,85 +2,41 @@ package models
 
 import (
 	"errors"
-	"strconv"
 	"time"
+
+	"github.com/astaxie/beego/orm"
 )
 
 var (
 	UserList map[string]*User
 )
 
-func init() {
-	UserList = make(map[string]*User)
-	u := User{"user_11111", "astaxie", "11111", Profile{"male", 20, "Singapore", "astaxie@gmail.com"}}
-	UserList["user_11111"] = &u
-}
-
 type User struct {
-	Id       string
-	Username string
-	Password string
-	Profile  Profile
+	Id       int
+	Name     string    `valid:"Required"`
+	Email    string    `orm:"unique" valid:"Required;Email"`
+	Username string    `orm:"unique" valid:"Required"`
+	Password string    `valid:"Required"`
+	Contact  string    `orm:"null"`
+	Address  string    `orm:"null;type(text)"`
+	Created  time.Time `orm:"auto_now_add;type(datetime)"`
 }
 
-type Profile struct {
-	Gender  string
-	Age     int
-	Address string
-	Email   string
+func (t *User) TableName() string {
+	return "users"
 }
 
-func AddUser(u User) string {
-	u.Id = "user_" + strconv.FormatInt(time.Now().UnixNano(), 10)
-	UserList[u.Id] = &u
-	return u.Id
+func init() {
+	orm.RegisterModel(new(User))
 }
 
-func GetUser(uid string) (u *User, err error) {
-	if u, ok := UserList[uid]; ok {
-		return u, nil
+func AddUser(u *User) (id int64, err error) {
+	o := orm.NewOrm()
+
+	id, err = o.Insert(u)
+	if err != nil {
+		return 0, errors.New("Failed insert data")
 	}
-	return nil, errors.New("User not exists")
-}
 
-func GetAllUsers() map[string]*User {
-	return UserList
-}
-
-func UpdateUser(uid string, uu *User) (a *User, err error) {
-	if u, ok := UserList[uid]; ok {
-		if uu.Username != "" {
-			u.Username = uu.Username
-		}
-		if uu.Password != "" {
-			u.Password = uu.Password
-		}
-		if uu.Profile.Age != 0 {
-			u.Profile.Age = uu.Profile.Age
-		}
-		if uu.Profile.Address != "" {
-			u.Profile.Address = uu.Profile.Address
-		}
-		if uu.Profile.Gender != "" {
-			u.Profile.Gender = uu.Profile.Gender
-		}
-		if uu.Profile.Email != "" {
-			u.Profile.Email = uu.Profile.Email
-		}
-		return u, nil
-	}
-	return nil, errors.New("User Not Exist")
-}
-
-func Login(username, password string) bool {
-	for _, u := range UserList {
-		if u.Username == username && u.Password == password {
-			return true
-		}
-	}
-	return false
-}
-
-func DeleteUser(uid string) {
-	delete(UserList, uid)
+	return id, nil
 }
