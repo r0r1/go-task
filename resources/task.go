b7fedff93ef8ca7cfb337f53eb94a9e9c9cc45c0
Rori Kurniadi <rori.kurniadi@yahoo.com>
2017-01-22 13:26:54 +0700

    remove libs

--- resources/task.go
+++ resources/task.go
@@ -6,8 +6,8 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
-	"github.com/rorikurniadi/go-task/libs"
 	"github.com/rorikurniadi/go-task/models"
+	"github.com/rorikurniadi/gor-pagination"
 )
 
 // NewUserStorage initializes the storage
